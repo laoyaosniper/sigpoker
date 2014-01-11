b68b88e5c26e4e1e31626473d2d7cdbf8692e8b3
Jack Jia <jackjia@umich.edu>
2014-01-11 08:49:12 -0500

    22

--- java/Contest2013/src/com/barracuda/contest2013/ContestBot.java
+++ java/Contest2013/src/com/barracuda/contest2013/ContestBot.java
@@ -62,7 +62,7 @@ public class ContestBot {
 							break;
 						}
 					}
-					index = 2>idx?idx:2;
+					index = 2>idx?2:idx;
 				}
 				else if(totalTime == 1){
 					index = 2;
@@ -79,7 +79,9 @@ public class ContestBot {
 				if((m.state.card>hand[hand.length-1])&&((m.state.card-hand[hand.length-1])>7)){
 					index = hand.length-1;
 				}
-				else if(findCard(hand,m.state.card)==hand.length-1&&(hand[minBigger(hand,m.state.card)]-m.state.card>6)){
+				else if((findCard(hand,m.state.card)==hand.length-1||
+						findCard(hand,m.state.card)==hand.length-2)
+						&&(hand[minBigger(hand,m.state.card)]-m.state.card>6)){
 					index = findCard(hand,m.state.card);
 				}
 				else{
@@ -97,7 +99,7 @@ public class ContestBot {
 		}
 	}
 	private int findCard(int[] hand, int card){
-		int index = -2;
+		int index = -3;
 		for(int i=0;i<hand.length;i++){
 			if(hand[i]==card){
 				index = i;
